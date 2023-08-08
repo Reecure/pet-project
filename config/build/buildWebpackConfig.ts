dd35d84b4481ccb08debbd7c7fb6aedf1ed36266
yura <ogogan45@gmail.com>
2023-08-08 09:07:21 +0300

    webpack config update

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -1,8 +1,8 @@
-import { BuildOptions } from './types/config';
-import { buildPlugins } from './buildPlugins';
-import { buildResolvers } from './buildResolvers';
-import { buildLoaders } from './buildLoaders';
-import { buildDevServer } from './buildDevServer';
+import {BuildOptions} from './types/config';
+import {buildPlugins} from './buildPlugins';
+import {buildResolvers} from './buildResolvers';
+import {buildLoaders} from './buildLoaders';
+import {buildDevServer} from './buildDevServer';
 
 export function BuildWebpackConfig(options: BuildOptions) {
     return {
@@ -18,6 +18,10 @@ export function BuildWebpackConfig(options: BuildOptions) {
         resolve: buildResolvers(options),
         module: {
             rules: buildLoaders(options),
+        },
+        performance: {
+            maxEntrypointSize: 512000,
+            maxAssetSize: 512000
         },
         devtool: options.isDev ? 'inline-source-map' : undefined,
         devServer: options.isDev ? buildDevServer(options) : undefined,
