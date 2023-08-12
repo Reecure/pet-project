bc31c0823c52675ef26925d733c357839e28b5ed
yura <ogogan45@gmail.com>
2023-08-12 22:45:23 +0300

    hot fix

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     },
     define: {
         __IS_DEV__: JSON.stringify(true),
-        __API__: JSON.stringify('http://localhost:8000'),
+        __API__: JSON.stringify('https://production-project-server-psi-ivory.vercel.app'),
         // 'https://production-project-server-psi-ivory.vercel.app'
         //http://localhost:8000
     },
